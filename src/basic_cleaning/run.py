4cbe37d8a2ab760c9bf25644da65c0d39956e24f
Qotov <alexander.o.kotov@gmail.com>
2021-11-24 23:11:51 +0100

    step BASIC_CLEANING added

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,8 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 import argparse
 import logging
 import wandb
+import pandas as pd
+
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -16,13 +18,31 @@ def go(args):
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    try:
+    	artifact_local_path = run.use_artifact(args.input_artifact).file()
+    	logger.info('We got artifact {}'.format(args.input_artifact))
+    except:
+    	logger.error('We did not get artifact {}'.format(args.input_artifact))
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # read and drop outliers
+    df = pd.read_csv(artifact_local_path)
+    df = df[df['price'].between(args.min_price, args.max_price)].copy()
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    # save dataframe
+    path_to_clean_df = 'clean_sample.csv'
+    df.to_csv(path_to_clean_df, index=False)
+    logger.info('Cleaned dataframe saved as {}'.format(path_to_clean_df))
+
+    out_artifact = wandb.Artifact(
+        name=args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    out_artifact.add_file(path_to_clean_df)
+
+    run.log_artifact(out_artifact)
+    logger.info('Dataframe uploaded in wandb')
 
 
 if __name__ == "__main__":
@@ -32,43 +52,43 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help='Input artifact name',
         required=True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help='Output artifact name',
         required=True
     )
 
     parser.add_argument(
         "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help='Type of artifact',
         required=True
     )
 
     parser.add_argument(
         "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help='Data after EDA',
         required=True
     )
 
     parser.add_argument(
         "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=float,
+        help='Min price for outliers removing',
         required=True
     )
 
     parser.add_argument(
         "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=float,
+        help='Max price for outliers removing',
         required=True
     )
 
