292796f6fb64ef43b9dc2bfc368684101ca47f89
Qotov <alexander.o.kotov@gmail.com>
2021-11-25 21:32:25 +0100

    Fix bug with input data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -28,6 +28,8 @@ def go(args):
     df = pd.read_csv(artifact_local_path)
     df = df[df['price'].between(args.min_price, args.max_price)].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # save dataframe
     path_to_clean_df = 'clean_sample.csv'
